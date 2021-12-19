164e0b3f8d496ff80fbc8e12475cf78af9af91a8
snew430 <seanpnew@hotmail.com>
2021-12-19 07:46:49 -0500

    added console logs to test code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!")
+var playerName = window.prompt("What is your robot's name?");
+
+// window.alert(playerName);
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10);
+console.log("Our robots name is " + playerName)
+
+// This creates a function called "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+
+// fight();
